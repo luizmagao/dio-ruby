8beffe01e8c7f4936d0767d1e7f07986fc00ba99
Luizmagao <luizmagao@gmail.com>
2024-12-05 00:56:16 -0300

    first commit

---
+++ index.rb
@@ -0,0 +1,8 @@
+puts "Informe seu nome"
+nome = gets.chomp
+puts "Informe seu sobrenome"
+sobrenome = gets.chomp
+puts "Informe sua idade"
+idade = gets.chomp
+
+puts "Meu nome é #{nome} #{sobrenome} e tenho #{idade} anos."
